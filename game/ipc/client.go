247b91cbf9287f33849f4fa460b0681994cbe261
nemowen <nemo.emails@gmail.com>
2014-03-25 23:21:57 +0800

    更新完善client.go程序

--- game/ipc/client.go
+++ game/ipc/client.go
@@ -12,3 +12,23 @@ func NewIPCClient(server *IPCServer) *IPCClient {
 	c := server.Connect()
 	return &IPCClient(c)
 }
+
+func (client *IPCClient) Call(method, params string) (resp *Response, err error) {
+	req := &Request(method, params)
+	var b []byte
+	b, err = json.Marshal(req)
+	if err != nil {
+		return
+	}
+
+	client.conn <- string(b)
+	str := <-client.conn //等待返回值
+
+	var resp1 Response
+	err = json.Unmarshal([]byte(str), &resp1)
+	resp = &resp1
+}
+
+func (client *IPCClient) Close() {
+	client.conn <- "CLOSE"
+}
